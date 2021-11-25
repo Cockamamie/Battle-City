92a3b48fb2bdae58de6967627968cf5ff8bcd1b6
mlgpudgeprohooker236 <xhbdlfkjgh@gmail.com>
2021-11-26 04:54:14 +0500

    added colliders and bounds

--- player.py
+++ player.py
@@ -1,10 +1,14 @@
+from typing import Tuple
+
+import pygame
+
 from Assets.sprites import SpritesCreator
-from directions import Direction
+from enums import Direction
 
 
 class Player:
-    def __init__(self, start: tuple[int, int]):
-        self.velocity = 2
+    def __init__(self, start: Tuple[int, int]):
+        self.velocity = 0.15
         self.position = start
 
         sprites_creator = SpritesCreator()
@@ -14,8 +18,12 @@ class Player:
         # self.three_stars = sprites_creator.three_stars_player()
         self.image = self.no_stars[Direction.Up]
 
-    def move(self, direction: Direction) -> None:
+    def move(self, direction: Direction, obstacles) -> None:
+        previous_pos = self.position
         delta_pos = [delta * self.velocity for delta in direction.value]
-        self.position = tuple([x + y for x, y in zip(self.position, delta_pos)])
+        self.position = [x + y for x, y in zip(self.position, delta_pos)]
+        intersecting_index = pygame.Rect.collidelist(pygame.Rect(self.position, (32, 32)), obstacles)
+        if intersecting_index != -1 or not (0 <= self.position[0] < 385 and 0 <= self.position[1] < 385):
+            self.position = previous_pos
         self.image = self.no_stars[direction]
     
