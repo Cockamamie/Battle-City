fe4eb70fafde716566e4522ea3688e338e1a64c3
Cockamamie <bugrymenko@yandex.ru>
2021-11-25 22:34:55 +0500

    Added image and rect initialization in all of blocks constructors

--- landscape.py
+++ landscape.py
@@ -1,22 +1,33 @@
+from pygame import Rect
 from interfaces import MapObject
+from Assets.sprites import SpritesCreator
 
 
 class Brick(MapObject):
-    pass
+    def __init__(self, position: tuple[int, int]):
+        self.image = SpritesCreator().brick()
+        self.rect = self.image.get_rect()
 
 
-class Concrete(MapObject):
-    pass
+class Steel(MapObject):
+    def __init__(self, position: tuple[int, int]):
+        self.image = SpritesCreator().steel()
+        self.rect = self.image.get_rect()
 
 
 class Grass(MapObject):
-    pass
+    def __init__(self, position: tuple[int, int]):
+        self.image = SpritesCreator().grass()
+        self.rect = self.image.get_rect()
 
 
 class Water(MapObject):
-    pass
+    def __init__(self, position: tuple[int, int]):
+        self.image = SpritesCreator().water()
+        self.rect = self.image.get_rect()
 
 
 class Ice(MapObject):
-    pass
-
+    def __init__(self, position: tuple[int, int]):
+        self.image = SpritesCreator().ice()
+        self.rect = self.image.get_rect()
