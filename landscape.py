226d64c9403252d833e4ce40f0854e63829b8b8c
mlgpudgeprohooker236 <xhbdlfkjgh@gmail.com>
2021-11-30 02:14:16 +0500

    added methods in walls classes to remove destroyed walls from screen

--- landscape.py
+++ landscape.py
@@ -12,12 +12,18 @@ class Brick(MapObject, pygame.sprite.Sprite):
         self.image = SpritesCreator().brick()
         self.rect = rect
 
+    def kill(self) -> None:
+        pygame.sprite.Sprite.kill(self)
+
 
 class Steel(MapObject, pygame.sprite.Sprite):
     def __init__(self, rect):
         pygame.sprite.Sprite.__init__(self)
         self.image = SpritesCreator().steel()
         self.rect = rect
+
+    def kill(self) -> None:
+        pygame.sprite.Sprite.kill(self)
 
 
 class Grass(MapObject, pygame.sprite.Sprite):
@@ -40,8 +46,3 @@ class Ice(MapObject, pygame.sprite.Sprite):
         self.image = SpritesCreator().ice()
         self.rect = rect
 
-
-class Empty(MapObject, pygame.sprite.Sprite):
-    def __init__(self, rect):
-        pygame.sprite.Sprite.__init__(self)
-        self.rect = rect
