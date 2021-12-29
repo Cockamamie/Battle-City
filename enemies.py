0f5552fe5cd92c8481025ff143501e8b36db5617
Cockamamie <bugrymenko@yandex.ru>
2021-12-30 02:58:49 +0500

    Armored tank sprite processing

--- enemies.py
+++ enemies.py
@@ -79,3 +79,8 @@ class Armored(Enemy):
             images = sprites_creator.armored_enemy(Color.Gray)
         self.images = [images, self._bonus_images][self._bonus_index]
 
+    def take_damage(self, explosion_queue, enemies, index, player):
+        super().take_damage(explosion_queue, enemies, index, player)
+        self.handle_sprite()
+
+
