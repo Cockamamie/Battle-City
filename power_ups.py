491e09ebe01b0ce4f97a0a8ca0af29cd9b941dfa
Cockamamie <bugrymenko@yandex.ru>
2021-12-30 02:11:47 +0500

    Added bonuses

--- power_ups.py
+++ power_ups.py
@@ -1,45 +1,65 @@
 from Assets.sprites import SpritesCreator
-from random import randint, choice
+from random import randrange, choice
+from pygame import Rect
+from abc import ABCMeta, abstractmethod
 
 sprites_creator = SpritesCreator()
 
 width = height = 32
-w = h = 416
 
 
-def spawn_random(self, bonuses):
-    pos = randint(0, 4) * w // 4, randint(0, 4) * h // 4
+def spawn_random():
+    pos = randrange(1, 11, 3) * width + width // 2,\
+          randrange(1, 11, 3) * height + height // 2
     power_up = choice(PowerUp.__subclasses__())
-    bonuses.append(power_up(pos))
+    bonus = power_up(pos)
+    return bonus
 
 
 class PowerUp:
-    def __init__(self, pos):
+    __metaclass = ABCMeta
+
+    def __init__(self, pos, image):
         self.position = pos
+        self.image = image
+        rect = image.get_rect()
+        self.rect = Rect(self.position[0], self.position[1], rect.width, rect.height)
+        self.is_visible = True
 
+    def switch_visibility(self):
+        self.is_visible = not self.is_visible
 
-class Timer(PowerUp):
-    def __init__(self, pos):
-        self.image = sprites_creator.timer()
-        super().__init__(pos)
-
-    def freeze(self, enemies):
+    @abstractmethod
+    def perform(self, player, enemies):
         pass
 
 
 class Star(PowerUp):
     def __init__(self, pos):
-        self.image = sprites_creator.star()
-        super().__init__(pos)
+        image = sprites_creator.star()
+        super().__init__(pos, image)
 
-    def upgrade(self):
+    def perform(self, player, enemies):
+        player.upgrade()
+
+
+class Grenade(PowerUp):
+    def __init__(self, pos):
+        image = sprites_creator.grenade()
+        super().__init__(pos, image)
+
+    def perform(self, player, enemies):
+        self.explode(enemies)
+
+    def explode(self, enemies):
         pass
 
 
 class HP(PowerUp):
     def __init__(self, pos):
-        self.image = sprites_creator.health()
-        super().__init__(pos)
+        image = sprites_creator.health()
+        super().__init__(pos, image)
 
-    def heal(self):
-        pass
+    def perform(self, player, enemies):
+        player.increase_hp()
+
